88c890f773b2c901174a199e2fa701efbdfb7513
Brayam Valero <bvalerop@gmail.com>
2022-12-09 10:26:51 -0400

    Feat: Adding Color Theme Button

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,12 +1,11 @@
 <template>
     <nav class="bg-gray-800 text-white p-3 flex">
-        <div class="pr-10">
-            My Blog
-        </div>
-        <ul>
-            <NuxtLink class="pr-5" to="/">Home</NuxtLink>
-            <NuxtLink class="pr-5" to="/about">About</NuxtLink>
+        <div class="pr-10">My Blog</div>
+        <div class="flex">
             <NuxtLink class="pr-5" to="/blog">Blog</NuxtLink>
-        </ul>
+            <NuxtLink class="pr-5" to="/about">About</NuxtLink>
+            <NuxtLink class="pr-5" to="/">Home</NuxtLink>
+            <ColorMode />
+        </div>
     </nav>
 </template>
