4b74035e163fe5a4d5ec4151fae2bb08b825dc11
Brayam Valero <bvalerop@gmail.com>
2023-09-17 13:51:13 -0400

    Revert "Feat: Updating nuxt version"
    This reverts commit bff66659b73270d050b82dd7ad7498646253142c.

--- pages/about.vue
+++ pages/about.vue
@@ -7,12 +7,7 @@ useHead({
 <template>
     <div class="About container">
         <section class="section-spacing">
-            <ContentDoc :path="$route.path">
-                <template v-slot="{ doc }">
-                    <ContentRenderer :value="doc" class="prose dark:prose-invert max-w-none" />
-                </template>
-                <template #not-found> <h1>Document not found</h1> </template>
-            </ContentDoc>
+            <ContentDoc class="prose dark:prose-invert max-w-none" />
         </section>
     </div>
 </template>
