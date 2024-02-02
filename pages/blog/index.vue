caabe6604b37d7cd07eda187d46b3a190eb22028
Brayam Valero <bvalerop@gmail.com>
2024-02-01 22:03:42 -0400

    Feat: Added extra styling to blog page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -40,12 +40,11 @@ useHead({
     <div class="Blog">
         <!-- Section -->
         <section class="container my-16">
-            <div class="flex flex-col items-center text-center mb-8">
+            <div class="flex flex-col items-center text-center">
                 <h1 class="text-white text-2xl font-bold mb-2">Blog</h1>
                 <p class="text-stone-400 font-light">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, ipsum.
                 </p>
-
                 <BaseInput
                     v-model="pagination.search"
                     type="text"
@@ -54,16 +53,26 @@ useHead({
                     class="w-80 mt-8"
                 />
             </div>
-            <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-6 my-8">
-                <CardPost v-for="post in posts" v-bind="post" />
+            <div :class="['my-16', posts.length < 1 ? 'flex justify-center text-center' : false]">
+                <template v-if="posts.length >= 1">
+                    <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-6">
+                        <CardPost v-for="post in posts" v-bind="post" />
+                    </div>
+                </template>
+                <template v-else>
+                    <div class="w-80 bg-stone-800 rounded-lg border border-stone-700 p-6">
+                        <h1 class="text-white text-2xl font-bold mb-2">Whoops!</h1>
+                        <p class="text-stone-400 font-light">Sin resultados</p>
+                    </div>
+                </template>
             </div>
-            <div>
-                <BasePagination
-                    v-model="pagination.currentPage"
-                    :per-page="pagination.perPage"
-                    :results-length="posts.length"
-                />
-            </div>
+
+            <!-- Pagination -->
+            <BasePagination
+                v-model="pagination.currentPage"
+                :per-page="pagination.perPage"
+                :results-length="posts.length"
+            />
         </section>
     </div>
 </template>
